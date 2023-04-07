14218596394be9b782e2ea2ae37b2204fda7739f
zhaolei <zhaolei@moqipobing.com>
2023-04-07 10:19:05 +0800

    feat: init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author : zhaolei
+ * @Date : Create in ${DATE} ${TIME}
+ * @Description :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
