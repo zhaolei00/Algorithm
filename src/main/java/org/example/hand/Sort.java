2e52f8be5e61e5f2b7114f2b208c5778da38f751
zhaolei <zhaolei@moqipobing.com>
2023-04-07 20:43:18 +0800

    feat: 排序

--- src/main/java/org/example/hand/Sort.java
+++ src/main/java/org/example/hand/Sort.java
@@ -57,6 +57,27 @@ public class Sort {
         }
     }
 
+    /**
+     * 插入排序
+     * 思路:
+     * 0 ~ 0 有序
+     * 0 ～ 1 有序
+     * 0 ～ 2 有序
+     *
+     * @param arr
+     */
+    private static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 1 && arr[j - 1] > arr[j]; j--) {
+                swap(arr, j - 1, j);
+            }
+        }
+    }
+
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
@@ -76,7 +97,7 @@ public class Sort {
     public static void main(String[] args) {
         int[] arr = {3, 6, 9, 4, 2, 3, 5, 8, 7, 9};
         printArr(arr);
-        bubbleSort(arr);
+        insertSort(arr);
         printArr(arr);
     }
 
