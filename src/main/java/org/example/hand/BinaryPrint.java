77b835eb75f972376345ae80a1d12e20038ab8e4
zhaolei <zhaolei@moqipobing.com>
2023-04-07 12:43:45 +0800

    feat: 算法知识

---
+++ src/main/java/org/example/hand/BinaryPrint.java
@@ -0,0 +1,21 @@
+package org.example.hand;
+
+/**
+ * @Author : zhaolei
+ * @Date : Create in 2023/4/7 12:33
+ * @Description : 二进制打印算法(13 = 00000000 00000000 00000000 00001101)
+ */
+public class BinaryPrint {
+
+    private static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        print(13);
+    }
+
+}
