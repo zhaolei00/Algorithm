a70763b51d8707773998a6155ade65148507d6e1
zhaolei <zhaolei@moqipobing.com>
2023-04-08 18:30:47 +0800

    feat: 二分

---
+++ src/main/java/org/example/hand/RightComparator.java
@@ -0,0 +1,12 @@
+package org.example.hand;
+
+/**
+ * @Author : zhaolei
+ * @Date : Create in 2023/4/8 14:42
+ * @Description :
+ *      对数器(正确的比较)
+ *          作用: 自己写的算法不知道是否正确，用对数据进行穷举，编写测试用例，进行测试。如果有不对的，找到不对的case进行调试，肯定能找到问题。
+ *              随机生成自己的测试样本，进行对比，如果有错误，可以进行调试改进。
+ */
+public class RightComparator {
+}
