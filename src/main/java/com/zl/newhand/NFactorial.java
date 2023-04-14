02e89b65ebd8a2c160c81615fffe694ab2a84c0a
zhaolei <zhaolei@moqipobing.com>
2023-04-14 11:34:09 +0800

    feat: 异或知识点

--- src/main/java/com/zl/newhand/NFactorial.java
+++ src/main/java/com/zl/newhand/NFactorial.java
@@ -7,33 +7,63 @@ package com.zl.newhand;
  */
 public class NFactorial {
 
-    private static int factorial(int num) {
+    /**
+     * N的阶乘
+     *
+     * @param N
+     * @return
+     */
+    private static int factorial(int N) {
         int res = 1;
-        for (int i = 1; i <= num; i++) {
+        for (int i = 1; i <= N; i++) {
             res *= i;
         }
         return res;
     }
 
-    private static int N_FACTORIAL(int n) {
+    /**
+     * int ans = 1! + 2! + 3! + N!
+     *
+     * @param N
+     * @return
+     */
+    private static int N_FACTORIAL(int N) {
         // 1! + 1!*2 + 2!*3
         int res = 0;
         int tmp = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             tmp = tmp * i;
             res += tmp;
         }
         return res;
     }
 
-    public static void main(String[] args) {
-        System.out.println(factorial(4));
-        // 1 + 2 + 6 + 24
-        System.out.println(N_FACTORIAL(4));
-    }
-
     //===============【题目】给定一个数组arr, 计算下标L到R位置上所有数的和。=====================
     // 思路: 前缀和数组，数组i位置是0到i位置所有数的和，所以计算下标L到R位置上所有数的和就是ans=[R]-[L-1],特殊情况，如果L为0，直接取[R]。
     // 前缀和数组就是一个辅助数组，帮助计算的。
+
+    private static int[] help = null;
+
+    // 计算L到R的和
+    private static int arrLRSum(int[] arr, int L, int R) {
+        if (arr == null || arr.length == 0 || L > R || L >= arr.length || R >= arr.length) {
+            return 0;
+        }
+        if (L == 0) {
+            return help[R];
+        }
+        return help[R] - help[L - 1];
+    }
+
+    // 生成辅助数组
+    private static void genHelpArr(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        help = new int[arr.length];
+        for (int i = 1; i < arr.length; i++) {
+            help[i] += help[i - 1];
+        }
+    }
 
 }
---
+++ src/main/java/com/zl/newhand/XOR.java
@@ -0,0 +1,79 @@
+package com.zl.newhand;
+
+import java.util.Arrays;
+
+/**
+ * @Author : zhaolei
+ * @Date : Create in 2023/4/14 09:25
+ * @Description :
+ */
+public class XOR {
+
+    //====【题目】给定arr数组, 不用额外变量，交换i位置和j位置的数=====================
+    // i和j位置相同会出错。
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] ^= arr[j];
+        arr[j] ^= arr[i];
+        arr[i] ^= arr[j];
+    }
+
+    //====【题目】给定arr数组, 有一种数出现了奇数次，其他数都出现偶数次，找到这个出现奇数次的数=====================
+    // 要求: 时间复杂度o(N), 额外空间复杂度o(1)
+    private static int findOddNumberTimesNumber(int[] arr) {
+        int ans = 0;
+        for (int i : arr) {
+            ans ^= i;
+        }
+        return ans;
+    }
+
+    //====【题目】给定一个int值a，提取出最右侧的1 =====================
+    // -a = ~a + 1
+    private static int question1(int a) {
+        return a & -a;
+    }
+
+    //====【题目】给定arr数组, 有两种数出现了奇数次，其他数都出现偶数次，找到这个出现奇数次的两种数=====================
+    // 要求: 时间复杂度o(N), 额外空间复杂度o(1)
+    private static int[] question2(int[] arr) {
+        int eor1 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            eor1 ^= arr[i];
+        }
+        int lastBit = eor1 & -eor1;
+        int eor2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if ((lastBit & arr[i]) == 0) {
+                eor2 ^= arr[i];
+            }
+        }
+        return new int[]{eor1 ^ eor2, eor2};
+    }
+
+    //====【题目】给定arr数组, 有一种数出现K次，其他数都出现M次，M>1, K<M, 找到出现K次的数=====================
+    // 要求: 时间复杂度o(N), 额外空间复杂度o(1)
+    // 步骤一: 初始化一个长度为32的int类型数组help
+    // 步骤二: 把arr数组中的数的位信息出现的次数，保存到help的对应位置
+    // 步骤三: 遍历help数组，如果位置上的数是M的整数倍，则出现K次的数在该位置不为1
+    private static int question3(int[] arr, int K, int M) {
+        int[] help = new int[32];
+        for (int num : arr) {
+            for (int j = 0; j < help.length; j++) {
+                help[j] += num >> j & 1;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < help.length; i++) {
+            if (help[i] % M != 0) {
+                ans |= 1 << i;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, -2, -3, 4, 5, -2, 1, -3, 4, 5, 1, -3, 4, 5, 1, -3, 4, 5, 1, -3, 4, 5};
+        System.out.println(question3(arr, 2, 5));
+    }
+
+}
