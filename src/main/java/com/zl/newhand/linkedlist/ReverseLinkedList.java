3ecd79d0dc520de24f9fc836a574a1ad73c124a2
zhaolei <zhaolei@moqipobing.com>
2023-04-14 13:31:59 +0800

    feat: 链表

--- src/main/java/com/zl/newhand/linkedlist/ReverseLinkedList.java
+++ src/main/java/com/zl/newhand/linkedlist/ReverseLinkedList.java
@@ -3,41 +3,15 @@ package com.zl.newhand.linkedlist;
 import lombok.Data;
 
 /**
- * @Author : zhaolei
- * @Date : Create in 2023/4/8 22:11
- * @Description : 反转链表相关算法
+ * 反转链表相关算法
  */
 public class ReverseLinkedList {
 
-    public static void main(String[] args) {
-        Node<Integer> node1 = new Node<>(1);
-        node1.pre = null;
-        Node<Integer> node2 = new Node<>(2);
-        node1.next = node2;
-        node2.pre = node1;
-        Node<Integer> node3 = new Node<>(3);
-        node2.next = node3;
-        node3.pre = node2;
-        Node<Integer> ans = new ReverseDoubleLinkedList<Integer>().reverse(node1);
-        Node<Integer> tail = null;
-        while (ans != null) {
-            System.out.print(ans.value + "->");
-            tail = ans;
-            ans = ans.next;
-        }
-        System.out.println("null");
-        while (tail != null) {
-            System.out.print(tail.value + "->");
-            tail = tail.pre;
-        }
-        System.out.println("null");
-    }
-
     //===============【题目】给定单向链表Head, 进行反转=====================
-    private static class ReverseSingleLinkedList<E> {
-        public Node<E> reverse(Node<E> head) {
-            Node<E> pre = null;
-            Node<E> next = null;
+    private static class ReverseSingleLinkedList {
+        public ListNode reverse(ListNode head) {
+            ListNode pre = null;
+            ListNode next = null;
             while (head != null) {
                 // head 记录要反转的节点
                 // pre 记录要反转的前一个节点
@@ -52,10 +26,10 @@ public class ReverseLinkedList {
     }
 
     //===============【题目】给定双向链表Head, 进行反转=====================
-    private static class ReverseDoubleLinkedList<E> {
-        public Node<E> reverse(Node<E> head) {
-            Node<E> pre = null;
-            Node<E> next = null;
+    private static class ReverseDoubleLinkedList {
+        public ListNode reverse(ListNode head) {
+            ListNode pre = null;
+            ListNode next = null;
             while (head != null) {
                 // head 记录要反转的节点
                 // pre 记录要反转的前一个节点
@@ -70,16 +44,49 @@ public class ReverseLinkedList {
         }
     }
 
+    //===============【题目】给定单向链表Head, 删除所有值为t的节点，返回头节点=====================
+    private static ListNode deleteNodeByValue(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode target = head;
+        while (target != null) {
+            if (target.val == val) {
+                if (pre == null) {
+                    head = target.next;
+                } else {
+                    pre.next = target.next;
+                }
+            } else {
+                pre = target;
+            }
+            target = target.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(9);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        deleteNodeByValue(head, 5);
+    }
+
     @Data
-    private static class Node<T> {
-        private T value;
+    private static class ListNode {
+        private int val;
 
-        private Node<T> pre;
+        private ListNode pre;
 
-        private Node<T> next;
+        private ListNode next;
 
-        public Node(T value) {
-            this.value = value;
+        public ListNode(int val) {
+            this.val = val;
         }
     }
 }
