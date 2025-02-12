ab06c11f13fa45daa25b39b7b238d574c4e2792d
zhaolei <zhaolei>
2025-02-12 19:11:39 +0800

    合并有序链表

--- src/main/java/com/zl/newhand/linkedlist/有序链表合并.java
+++ src/main/java/com/zl/newhand/linkedlist/有序链表合并.java
@@ -17,49 +17,25 @@ public class 有序链表合并 {
         if (head1 == null || head2 == null) {
             return head1 == null ? head2 : head1;
         }
-        Node<Integer> minHead = length(head1) > length(head2) ? head2 : head1;
-        Node<Integer> maxHead = minHead == head1 ? head2 : head1;
-        Node<Integer> ans = null;
-        if (minHead.getV() <= maxHead.getV()) {
-            ans = minHead;
-            minHead = minHead.getNext();
-        } else {
-            ans = maxHead;
-            maxHead = maxHead.getNext();
-        }
-        Node<Integer> cur; // 当前处理的节点。
+        Node<Integer> ans = head1.getV() <= head2.getV() ? head1 : head2;
+        // 小头
+        Node<Integer> minHead = ans.getNext();
+        // 大头
+        Node<Integer> maxHead = ans == head1 ? head2 : head1;
         Node<Integer> last = ans; // 处理好的最后一个节点
         // 两个都不为空
         while (minHead != null && maxHead != null) {
             if (minHead.getV() <= maxHead.getV()) {
-                cur = minHead;
+                last.setNext(minHead);
                 minHead = minHead.getNext();
             } else {
-                cur = maxHead;
+                last.setNext(maxHead);
                 maxHead = maxHead.getNext();
             }
-            last.setNext(cur);
-            last = cur;
+            last = last.getNext();
         }
-        if (minHead != null) {
-            last.setNext(minHead);
-        }
-        if (maxHead != null) {
-            last.setNext(maxHead);
-        }
+        last.setNext(minHead != null ? minHead : maxHead);
         return ans;
-    }
-
-    private static int length(Node<Integer> head) {
-        if (head == null) {
-            return 0;
-        }
-        int size = 0;
-        while (head != null) {
-            size++;
-            head = head.getNext();
-        }
-        return size;
     }
 
     private static void test() {
