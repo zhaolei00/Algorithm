843fc268214fb2012aac45119ba1553189ee0eb6
zhaolei <zhaolei>
2025-02-13 13:11:08 +0800

    树的遍历

--- src/main/java/com/zl/newhand/tree/树的遍历.java
+++ src/main/java/com/zl/newhand/tree/树的遍历.java
@@ -5,7 +5,31 @@ package com.zl.newhand.tree;
 public class 树的遍历 {
 
     // 先序遍历 先父
+    public static void xian(TreeNode<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.value);
+        xian(head.left);
+        xian(head.right);
+    }
     // 中序遍历 中父
+    public static void zhong(TreeNode<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        zhong(head.left);
+        System.out.println(head.value);
+        zhong(head.right);
+    }
     // 后序遍历 后父
+    public static void hou(TreeNode<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        hou(head.left);
+        hou(head.right);
+        System.out.println(head.value);
+    }
 
 }
