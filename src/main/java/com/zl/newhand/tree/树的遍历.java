37237cac2e27015e09f70a7dd11707bc9b114bee
zhaolei <zhaolei>
2025-02-13 14:49:51 +0800

    树的遍历

--- src/main/java/com/zl/newhand/tree/树的遍历.java
+++ src/main/java/com/zl/newhand/tree/树的遍历.java
@@ -1,7 +1,9 @@
 package com.zl.newhand.tree;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 /**
  */
@@ -52,5 +54,54 @@ public class 树的遍历 {
                 deque.addLast(treeNode.right);
             }
         }
+    }
+
+    // 层序遍历
+    // 思路: 因为返回值是每次节点的list，所以需要两个双端队列，来不断变化。
+    public static List<List<Integer>> ceng1(TreeNode<Integer> root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Deque<TreeNode<Integer>> deque1 = new ArrayDeque<>();
+        Deque<TreeNode<Integer>> deque2 = new ArrayDeque<>();
+        deque1.addLast(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        while (!deque1.isEmpty()) {
+            TreeNode<Integer> node = deque1.pollFirst();
+            if (node.left != null) {
+                deque2.addLast(node.left);
+            }
+            if (node.right != null) {
+                deque2.addLast(node.right);
+            }
+            mid.add(node.value);
+            if (deque1.isEmpty()) {
+                Deque<TreeNode<Integer>> temp = deque1;
+                deque1 = deque2;
+                deque2 = temp;
+                ans.add(mid);
+                mid = new ArrayList<>();
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(4);
+        TreeNode<Integer> a = new TreeNode<>(8);
+        TreeNode<Integer> b = new TreeNode<>(1);
+        root.left = a;
+        root.right = b;
+        TreeNode<Integer> c = new TreeNode<>(3);
+        TreeNode<Integer> d = new TreeNode<>(5);
+        a.left = c;
+        a.right = d;
+        TreeNode<Integer> e = new TreeNode<>(7);
+        TreeNode<Integer> f = new TreeNode<>(2);
+        b.left = e;
+        b.right = f;
+        List<List<Integer>> lists = ceng1(root);
+        System.out.println(lists);
     }
 }
