8b046967328ca1a3497af2e21af135c9ab0be2c7
zhaolei <zhaolei>
2025-02-13 15:51:18 +0800

    判断镜面树

---
+++ src/main/java/com/zl/newhand/tree/判断一个树是否为镜面树.java
@@ -0,0 +1,23 @@
+package com.zl.newhand.tree;
+
+/**
+ */
+public class 判断一个树是否为镜面树 {
+
+    //===============【题目】判断一棵树是否是镜面树(对称树)=====================
+    public static boolean isMirrorTree(TreeNode<Integer> root) {
+        return isMirror(root, root);
+
+    }
+
+    private static boolean isMirror(TreeNode<Integer> root1, TreeNode<Integer> root2) {
+        if (root1 == null ^ root2 == null) {
+            return false;
+        }
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        return root1.value.equals(root2.value) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+    }
+
+}
