499c5efb0bdb2cbdcb0444e820b0a3c2c8c96038
zhaolei <zhaolei>
2025-02-16 13:16:29 +0800

    数组实现队列

--- src/main/java/com/zl/linkedlist/_8_数组实现队列.java
+++ src/main/java/com/zl/linkedlist/_8_数组实现队列.java
@@ -1,5 +1,6 @@
 package com.zl.linkedlist;
 
+import com.zl.linkedlist.interfaces.Deque;
 import com.zl.linkedlist.interfaces.Queue;
 
 /**
@@ -60,22 +61,84 @@ public class _8_数组实现队列 {
         }
     }
 
-    public static void main(String[] args) {
-        MyQueue myQueue = new MyQueue(3);
-        myQueue.offer(4);
-        myQueue.offer(7);
-        myQueue.offer(6);
-        while (!myQueue.isEmpty()) {
-            System.out.println(myQueue.poll());
-        }
-        myQueue.offer(1);
-        myQueue.offer(3);
-        myQueue.offer(2);
-        while (!myQueue.isEmpty()) {
-            System.out.println(myQueue.poll());
+    public static class MyDeque implements Deque<Integer> {
+
+        private int[] data;
+
+        private int left = 0;
+
+        private int right = 1;
+
+        private int size;
+
+        public MyDeque(int size) {
+            this.data = new int[size];
         }
 
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public void firstOffer(Integer val) {
+            if (size == data.length) {
+                throw new IllegalArgumentException();
+            }
+            data[left] = val;
+            left = left == 0 ? data.length - 1: left - 1;
+            size++;
+        }
+
+        @Override
+        public void lastOffer(Integer val) {
+            if (size == data.length) {
+                throw new IllegalArgumentException();
+            }
+            data[right] = val;
+            right = right == data.length - 1 ? 0 : right + 1;
+            size++;
+        }
+
+        @Override
+        public Integer firstPoll() {
+            if (size == 0) {
+                throw new IllegalArgumentException();
+            }
+            int index = left == data.length - 1 ? 0 : left + 1;
+            left = index;
+            size--;
+            return data[index];
+        }
+
+        @Override
+        public Integer lastPoll() {
+            if (size == 0) {
+                throw new IllegalArgumentException();
+            }
+            int index = right == 0 ? data.length - 1 : right - 1;
+            right = index;
+            size--;
+            return data[index];
+        }
 
     }
 
+    public static void main(String[] args) {
+        Deque<Integer> deque = new MyDeque(10);
+        deque.lastOffer(1);
+        deque.firstOffer(3);
+        deque.firstOffer(5);
+        deque.lastOffer(6);
+        deque.lastOffer(9);
+        int size1 = deque.size();
+        for (int i = 0; i < size1; i++) {
+            System.out.println(deque.firstPoll());
+        }
+    }
 }
