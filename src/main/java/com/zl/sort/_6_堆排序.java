7285737634c0f73eae97e027ec9aae42e3d1440b
zhaolei <zhaolei>
2025-02-20 20:46:41 +0800

    堆排序

--- src/main/java/com/zl/sort/_6_堆排序.java
+++ src/main/java/com/zl/sort/_6_堆排序.java
@@ -1,6 +1,75 @@
 package com.zl.sort;
 
+import com.zl.tiku._4_随机数概率问题;
+import com.zl.tiku._99_对数器;
+
+import java.util.Arrays;
+
 /**
+ * 堆排序只能用大根堆，小根堆在排序过程中，前面小的确定完，如果不移动，没办法表示成完全二叉树。
  */
 public class _6_堆排序 {
+
+    /**
+     * 时间复杂度: o(N*logN)
+     * @param arr
+     */
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            // 上浮
+            int targetIndex = i;
+            while (arr[targetIndex] > arr[(targetIndex - 1) / 2]) {
+                swap(arr, targetIndex, (targetIndex - 1) / 2);
+                targetIndex = (targetIndex - 1) / 2;
+            }
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i); // 最大数和最后面的交换，相当于取出0位置的数
+            int targetIndex = 0;
+            int left = 1;
+            while (left < i) {
+                int maxChile = left + 1 < i && arr[left + 1] > arr[left] ? left + 1 : left;
+                if (arr[maxChile] <= arr[targetIndex]) {
+                    break;
+                }
+                swap(arr, targetIndex, maxChile);
+                targetIndex = maxChile;
+                left = targetIndex * 2 + 1;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("测试开始");
+        int times= 100000;
+        int maxValues = 1000;
+        int maxLength = 1000;
+        for (int i = 0; i < times; i++) {
+            int[] arr = _99_对数器.randomGenIntArray(maxLength, maxValues);
+            int[] arr1 = _99_对数器.copyArray(arr);
+            int[] arr2 = _99_对数器.copyArray(arr);
+            if (arr == null) {
+                continue;
+            }
+            heapSort(arr1);
+            Arrays.sort(arr2);
+            if (!_99_对数器.arrayEquals(arr1, arr2)) {
+                System.out.println("fail:" + Arrays.toString(arr));
+                System.out.println("fail:" + Arrays.toString(arr1));
+                System.out.println("fail:" + Arrays.toString(arr2));
+                return;
+            }
+        }
+        System.out.println("Nice");
+    }
+
 }
