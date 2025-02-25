5452ac126876be1392397baf81e617fdf2578564
zhaolei <zhaolei>
2025-02-25 17:44:50 +0800

    _4_2_树的遍历非递归

--- src/main/java/com/zl/tree/_4_2_树的遍历非递归.java
+++ src/main/java/com/zl/tree/_4_2_树的遍历非递归.java
@@ -3,6 +3,7 @@ package com.zl.tree;
 import java.util.Stack;
 
 /**
+ *
  */
 public class _4_2_树的遍历非递归 {
 
@@ -20,6 +21,24 @@ public class _4_2_树的遍历非递归 {
             }
             if (pop.left != null) {
                 stack.push(pop.left);
+            }
+        }
+    }
+
+    public void zhong(TreeNode cur) {
+        if (cur == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        // 把左孩子都压入栈，直到null。
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                System.out.println(cur.val);
+                cur = cur.right;
             }
         }
     }
