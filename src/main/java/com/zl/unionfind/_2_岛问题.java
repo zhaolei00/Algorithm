88ad954e03490ca31744508ea0dfd029ed6ecc45
zhaolei <zhaolei>
2025-04-11 17:21:38 +0800

    并查集

--- src/main/java/com/zl/unionfind/_2_岛问题.java
+++ src/main/java/com/zl/unionfind/_2_岛问题.java
@@ -1,10 +1,12 @@
 package com.zl.unionfind;
 
+import java.util.List;
+
 /**
  */
 public class _2_岛问题 {
 
-    // 值为'1'代表有值
+    // 给定一个二维数组，值为'1'代表有值。求岛数量
     // 有两种思路:
     //      第一种: 感染方式
     //      第二种: 并查集方式
@@ -12,4 +14,9 @@ public class _2_岛问题 {
         return 0;
     }
 
+    // 给定一个m*n的二维数组，positions是一个[[0,1],[3,4],[4,3]]这样的二维数组，代表此位置为1。
+    // 求每加一个位置，此时的岛数量。
+    public static List<Integer> landsNum2(int m, int n, int[][] positions) {
+        return null;
+    }
 }
